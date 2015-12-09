7d5681a1aac555ea509009edd6b2fa3c88e6dcf0
misc <MiSc@ORD01777.ordina.belgium>
2015-12-09 09:58:55 +0100

    Java doc

--- common.moplaf.emf/com.misc.common.moplaf.emf.edit/src/com/misc/common/moplaf/emf/edit/ui/provider/TransientItemProviderAdapter.java
+++ common.moplaf.emf/com.misc.common.moplaf.emf.edit/src/com/misc/common/moplaf/emf/edit/ui/provider/TransientItemProviderAdapter.java
@@ -15,6 +15,38 @@ import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
+/**
+ * <p>
+ * Manage intermediary nodes in the tree of objects provided by the interface ITreeItemContentProvider. 
+ * This allows to implement nodes giving as children a subset of the children of the concrete parent object.
+ * <p>
+ * Say we have a 'Parent' object, and that we want a 'Children' node, having himself as children a subset 
+ * of the children of 'Parent'. TransientItemProviderAdapter allows us to implement this node 'Children' as follows
+ * <ul>
+ * <li>Extends TransientItemProviderAdapter as for instance ChildrenItemAdapter, in this new class
+ *   <ul>
+ *   <li>Add this ChildrenItemAdapter to the adapters of 'Parent', e.g. in the constructor of this ChildrenItemAdapter </li>
+ *   <li>Override getResourceLocator and return the instance of the Plugin of 'Parent'</li>
+ *   <li>Override getChildrenFeatures and return the children features to be used for the children of this ChildrenItemAdapter</li>
+ *   <li>Override getText and return the label of node 'Children'</li>
+ *   </ul> 
+ * </li>
+ * <li>In the Parent class
+ *   <ul>
+ *   <li>Override getChildren and return an instance of this ChildrenItemAdapter</li>
+ *   <li>Implement a getter for this ChildrenItemAdapter</li>
+ *   <li>Make this adapter stateful</li>
+  *  </ul> 
+ * </li>
+ * <li>In the Child class
+ *   <ul>
+ *   <li>Override getParent and return this ChildrenItemAdapter, accessed via the gette in the Parent class</li>
+ *   </ul> 
+ * </li>
+ * </ul> 
+ * <p>
+ * @author michel
+ */
 
 public class TransientItemProviderAdapter extends ItemProviderAdapter
 		implements IEditingDomainItemProvider, 
