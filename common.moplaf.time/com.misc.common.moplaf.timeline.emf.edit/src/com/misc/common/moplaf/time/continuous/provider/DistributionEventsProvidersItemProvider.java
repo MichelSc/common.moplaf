0b269450587469e9f073af85426e2ef9defe3703
misc <MiSc@ORD01777.ordina.belgium>
2016-01-18 09:10:05 +0100

    Refresh node content

--- common.moplaf.time/com.misc.common.moplaf.timeline.emf.edit/src/com/misc/common/moplaf/time/continuous/provider/DistributionEventsProvidersItemProvider.java
+++ common.moplaf.time/com.misc.common.moplaf.timeline.emf.edit/src/com/misc/common/moplaf/time/continuous/provider/DistributionEventsProvidersItemProvider.java
@@ -3,7 +3,9 @@ package com.misc.common.moplaf.time.continuous.provider;
 import java.util.Collection;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import com.misc.common.moplaf.time.continuous.ContinuousPackage;
 import com.misc.common.moplaf.time.continuous.Distribution;
@@ -28,4 +30,29 @@ public class DistributionEventsProvidersItemProvider extends DistributionNodesIt
 	public String getText(Object object) {
 		return "Event Providers";
 	}
+
+@Override
+public void notifyChanged(Notification notification) {
+	updateChildren(notification);
+
+	switch (notification.getFeatureID(Distribution.class)) {
+//		case ContinuousPackage.DISTRIBUTION__TIME_UNIT:
+//		case ContinuousPackage.DISTRIBUTION__HORIZON_START:
+//		case ContinuousPackage.DISTRIBUTION__HORIZON_END:
+//		case ContinuousPackage.DISTRIBUTION__CHILD_EVENTS:
+//		case ContinuousPackage.DISTRIBUTION__SEQUENCE_EVENTS:
+//		case ContinuousPackage.DISTRIBUTION__PARENT_DISTRIBUTION:
+//		case ContinuousPackage.DISTRIBUTION__CHILD_DISTRIBUTION:
+//			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+//			return;
+		case ContinuousPackage.DISTRIBUTION__EVENTS_PROVIDERS:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), 
+					          true,     // content refresh 
+					          false));  // label update
+			return; 
+	}
+	super.notifyChanged(notification);
+}
+
+
 }
