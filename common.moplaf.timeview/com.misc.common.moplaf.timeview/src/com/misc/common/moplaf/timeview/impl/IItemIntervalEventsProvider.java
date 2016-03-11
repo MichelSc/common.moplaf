fef481236ac5f9a9322bea64f7012117c4427d1d
misc <MiSc@ORD01777.ordina.belgium>
2016-03-11 14:50:20 +0100

    Write doc

--- common.moplaf.timeview/com.misc.common.moplaf.timeview/src/com/misc/common/moplaf/timeview/impl/IItemIntervalEventsProvider.java
+++ common.moplaf.timeview/com.misc.common.moplaf.timeview/src/com/misc/common/moplaf/timeview/impl/IItemIntervalEventsProvider.java
@@ -2,6 +2,30 @@ package com.misc.common.moplaf.timeview.impl;
 
 import java.util.Collection;
 
+/**
+ * <p>
+ * This interface declare the method(s) to be supported by an object that the Gantt must show as a Row
+ * <p>
+ * To implement this interface by an {@link ItemProviderAdapter}, in the concrete class, do the following 
+ * <ul>
+ *   <li>add <code>@implements IItemIntervalEventsProvider</code> in the user doc</li>
+ *   <li>regenerate the code</li>
+ *   <li>add the unimplemented methods</li>
+ *   <li>implement {@link #getIntervalEvents(Object)}</li>
+ *   <li>override {@link #isAdapterForType(Object)} and return true when the type is IItemIntervalEventsProvider</li>
+ * </ul> 
+ * In the project's ProviderAdapterFactory, you may need to change 
+ * <ul>
+ *   <li>the method {@link #isFactoryForType}</li>
+ * </ul> 
+ * In the plugin.xml,  
+ * <ul>
+ *   <li>you may need to add the type IItemIntervalEventsProvider in the field SupportedTypes for extension org.eclipse.emf.edit.itemProviderAdapterFactories </li>
+ * </ul> 
+ * <p>
+ * @author michel
+ */
+
 public interface IItemIntervalEventsProvider {
 	Collection<?> getIntervalEvents(Object element);
 }
