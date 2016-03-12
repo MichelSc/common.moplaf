0742b44edff78ced88458bfbbf08fe64f77942ac
michel <michel@miscpc01>
2016-03-12 14:02:58 +0100

    First version

---
+++ common.mopalf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/emf/Util.java
@@ -0,0 +1,20 @@
+package com.misc.common.moplaf.emf;
+
+import org.eclipse.swt.graphics.RGB;
+
+public class Util {
+	public static RGB integerToRgb(int integer){
+    	int b = integer % 256;
+    	int rest = (integer-b)/256;
+    	int g = rest % 256;
+    	int r = rest / 256;
+    	return new RGB(r, g, b);
+	}
+	
+	public static int rgbToInt(RGB rgb){
+		if ( rgb == null) { return 0; }
+        int toReturn = 256 * ( 256 * rgb.red + rgb.green )+ rgb.blue;
+        return toReturn;
+	}
+
+}
