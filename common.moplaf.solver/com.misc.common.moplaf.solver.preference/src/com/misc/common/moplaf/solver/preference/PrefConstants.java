ac7375bb85bfea50e048354ff214b55c73e2740c
michel <michel@miscpc01>
2016-04-10 17:17:47 +0200

    IMplement preferences

---
+++ common.moplaf.solver/com.misc.common.moplaf.solver.preference/src/com/misc/common/moplaf/solver/preference/PrefConstants.java
@@ -0,0 +1,9 @@
+package com.misc.common.moplaf.solver.preference;
+
+public interface PrefConstants {
+	
+	String PREF_LOG_ON_INFO    = "com.misc.common.moplaf.solver.Preference.logOnInfo";
+	String PREF_LOG_ON_WARNING = "com.misc.common.moplaf.solver.Preference.logOnWarning";
+	String PREF_LOG_ON_ERROR   = "com.misc.common.moplaf.solver.Preference.logOnError";
+
+}
---
+++ common.moplaf.solver/com.misc.common.moplaf.solver.preference/src/com/misc/common/moplaf/solver/preference/PreferencePage.java
@@ -0,0 +1,26 @@
+package com.misc.common.moplaf.solver.preference;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage, PrefConstants {
+
+	public PreferencePage() {
+		super(GRID);
+	}
+	
+	@Override
+	public void init(IWorkbench workbench) {
+		this.setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		this.addField(new BooleanFieldEditor(PREF_LOG_ON_INFO   , "Log infos:"   , this.getFieldEditorParent()));
+		this.addField(new BooleanFieldEditor(PREF_LOG_ON_WARNING, "Log warnings:", this.getFieldEditorParent()));
+		this.addField(new BooleanFieldEditor(PREF_LOG_ON_ERROR  , "Log errors:"  , this.getFieldEditorParent()));
+	}
+
+}
