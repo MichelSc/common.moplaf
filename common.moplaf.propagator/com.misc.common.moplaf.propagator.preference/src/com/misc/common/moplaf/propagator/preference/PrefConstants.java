9952a28dbe349c80e81bb7d165debeab90a809fd
michel <michelschaffers@gmail.com>
2016-06-10 08:53:12 +0200

    Commit from commit window

--- common.moplaf.propagator/com.misc.common.moplaf.propagator.preference/src/com/misc/common/moplaf/propagator/preference/PrefConstants.java
+++ common.moplaf.propagator/com.misc.common.moplaf.propagator.preference/src/com/misc/common/moplaf/propagator/preference/PrefConstants.java
@@ -1,7 +1,6 @@
 package com.misc.common.moplaf.propagator.preference;
 
 public interface PrefConstants {
-	
 	String PREF_LOG_ON_TOUCH   = "com.misc.common.moplaf.propagator.Preference.logOnTouch";
 	String PREF_LOG_ON_CALC    = "com.misc.common.moplaf.propagator.Preference.logOnCalc";
 	String PREF_LOG_ON_INFO    = "com.misc.common.moplaf.propagator.Preference.logOnInfo";
