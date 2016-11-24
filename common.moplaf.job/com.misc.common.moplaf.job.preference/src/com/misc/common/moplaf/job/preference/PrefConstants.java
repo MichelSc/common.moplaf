ebff24b2d12924f6ad57c12aed1c3a0f89f4709a
MiSc <MiSc@ORD02652.ordina.belgium>
2016-11-24 14:05:47 +0100

    One more

---
+++ common.moplaf.job/com.misc.common.moplaf.job.preference/src/com/misc/common/moplaf/job/preference/PrefConstants.java
@@ -0,0 +1,10 @@
+package com.misc.common.moplaf.job.preference;
+
+public interface PrefConstants {
+	
+//	String PREF_LOG_ON_INFO    = "com.misc.common.moplaf.job.Preference.logOnInfo";
+//	String PREF_LOG_ON_WARNING = "com.misc.common.moplaf.job.Preference.logOnWarning";
+//	String PREF_LOG_ON_ERROR   = "com.misc.common.moplaf.job.Preference.logOnError";
+	String PREF_SHOW_METADATA  = "com.misc.common.moplaf.job.Preference.showMetadata";
+
+}
---
+++ common.moplaf.job/com.misc.common.moplaf.job.preference/src/com/misc/common/moplaf/job/preference/PreferencePage.java
@@ -0,0 +1,27 @@
+package com.misc.common.moplaf.job.preference;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage, PrefConstants {
+
+	public PreferencePage() {
+		super(GRID);
+	}
+	
+	@Override
+	public void init(IWorkbench workbench) {
+		this.setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	protected void createFieldEditors() {
+//		this.addField(new BooleanFieldEditor(PREF_LOG_ON_INFO   , "Log infos:"    , this.getFieldEditorParent()));
+//		this.addField(new BooleanFieldEditor(PREF_LOG_ON_WARNING, "Log warnings:" , this.getFieldEditorParent()));
+//		this.addField(new BooleanFieldEditor(PREF_LOG_ON_ERROR  , "Log errors:"   , this.getFieldEditorParent()));
+		this.addField(new BooleanFieldEditor(PREF_SHOW_METADATA , "Show metadata:", this.getFieldEditorParent()));
+	}
+
+}
