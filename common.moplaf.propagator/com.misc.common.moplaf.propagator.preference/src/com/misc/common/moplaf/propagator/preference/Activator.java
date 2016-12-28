a178133544beb36eff8ff7e8d0c0ca1b52064239
michel <michelschaffers@gmail.com>
2016-12-28 16:52:25 +0100

    Initialize preferences

--- common.moplaf.propagator/com.misc.common.moplaf.propagator.preference/src/com/misc/common/moplaf/propagator/preference/Activator.java
+++ common.moplaf.propagator/com.misc.common.moplaf.propagator.preference/src/com/misc/common/moplaf/propagator/preference/Activator.java
@@ -3,17 +3,11 @@ package com.misc.common.moplaf.propagator.preference;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-import com.misc.common.moplaf.propagator2.Plugin;
-
-
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin implements PrefConstants{
+public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.misc.common.moplaf.propagator.preference"; //$NON-NLS-1$
@@ -34,7 +28,6 @@ public class Activator extends AbstractUIPlugin implements PrefConstants{
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		this.onStartUp();
 	}
 
 	/*
@@ -46,45 +39,6 @@ public class Activator extends AbstractUIPlugin implements PrefConstants{
 		super.stop(context);
 	}
 	
-	public void onStartUp(){
-
-		final IPreferenceStore prefStore = Activator.getDefault().getPreferenceStore();
-		boolean logOnTouch   = prefStore.getBoolean(PREF_LOG_ON_TOUCH);
-		boolean logOnCalc    = prefStore.getBoolean(PREF_LOG_ON_CALC);
-		boolean logOnInfo    = prefStore.getBoolean(PREF_LOG_ON_INFO);
-		boolean logOnWarning = prefStore.getBoolean(PREF_LOG_ON_WARNING);
-		boolean logOnError   = prefStore.getBoolean(PREF_LOG_ON_ERROR);
-		
-		Plugin.INSTANCE.getLogger().setLogOnTouch  (logOnTouch);
-		Plugin.INSTANCE.getLogger().setLogOnCalc   (logOnCalc);
-		Plugin.INSTANCE.getLogger().setLogOnInfo   (logOnInfo);
-		Plugin.INSTANCE.getLogger().setLogOnWarning(logOnWarning);
-		Plugin.INSTANCE.getLogger().setLogOnError  (logOnError);
-	
-		prefStore.addPropertyChangeListener(new IPropertyChangeListener() {
-	      public void propertyChange(PropertyChangeEvent event) {
-	    	  String property = event.getProperty();
-	    	  Object newValue = event.getNewValue();
-	    	  
-	    	  Boolean newValueAsBoolean = false;
-	    	  if ( newValue instanceof Boolean ){
-	    		  newValueAsBoolean = (Boolean)newValue;
-	    	  }
-
-	    	  if ( property == PREF_LOG_ON_TOUCH ){
-	    			Plugin.INSTANCE.getLogger().setLogOnTouch  (newValueAsBoolean);
-	    	  } else if ( property == PREF_LOG_ON_CALC){
-	    			Plugin.INSTANCE.getLogger().setLogOnCalc   (newValueAsBoolean);
-	    	  } else if ( property == PREF_LOG_ON_INFO ){
-	    			Plugin.INSTANCE.getLogger().setLogOnInfo   (newValueAsBoolean);
-	    	  } else if ( property == PREF_LOG_ON_WARNING ){
-	    			Plugin.INSTANCE.getLogger().setLogOnWarning(newValueAsBoolean);
-	    	  } else if ( property == PREF_LOG_ON_ERROR ){
-	    			Plugin.INSTANCE.getLogger().setLogOnError  (newValueAsBoolean);
-	    	  }
-	       }});
-	}
-
 
 	/**
 	 * Returns the shared instance
---
+++ common.moplaf.propagator/com.misc.common.moplaf.propagator.preference/src/com/misc/common/moplaf/propagator/preference/Startup.java
@@ -0,0 +1,51 @@
+package com.misc.common.moplaf.propagator.preference;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.ui.IStartup;
+
+import com.misc.common.moplaf.propagator2.Plugin;
+
+public class Startup implements IStartup, PrefConstants {
+
+	@Override
+	public void earlyStartup() {
+		final IPreferenceStore prefStore = Activator.getDefault().getPreferenceStore();
+		boolean logOnTouch   = prefStore.getBoolean(PREF_LOG_ON_TOUCH);
+		boolean logOnCalc    = prefStore.getBoolean(PREF_LOG_ON_CALC);
+		boolean logOnInfo    = prefStore.getBoolean(PREF_LOG_ON_INFO);
+		boolean logOnWarning = prefStore.getBoolean(PREF_LOG_ON_WARNING);
+		boolean logOnError   = prefStore.getBoolean(PREF_LOG_ON_ERROR);
+		
+		Plugin.INSTANCE.getLogger().setLogOnTouch  (logOnTouch);
+		Plugin.INSTANCE.getLogger().setLogOnCalc   (logOnCalc);
+		Plugin.INSTANCE.getLogger().setLogOnInfo   (logOnInfo);
+		Plugin.INSTANCE.getLogger().setLogOnWarning(logOnWarning);
+		Plugin.INSTANCE.getLogger().setLogOnError  (logOnError);
+	
+		prefStore.addPropertyChangeListener(new IPropertyChangeListener() {
+	      public void propertyChange(PropertyChangeEvent event) {
+	    	  String property = event.getProperty();
+	    	  Object newValue = event.getNewValue();
+	    	  
+	    	  Boolean newValueAsBoolean = false;
+	    	  if ( newValue instanceof Boolean ){
+	    		  newValueAsBoolean = (Boolean)newValue;
+	    	  }
+
+	    	  if ( property == PREF_LOG_ON_TOUCH ){
+	    			Plugin.INSTANCE.getLogger().setLogOnTouch  (newValueAsBoolean);
+	    	  } else if ( property == PREF_LOG_ON_CALC){
+	    			Plugin.INSTANCE.getLogger().setLogOnCalc   (newValueAsBoolean);
+	    	  } else if ( property == PREF_LOG_ON_INFO ){
+	    			Plugin.INSTANCE.getLogger().setLogOnInfo   (newValueAsBoolean);
+	    	  } else if ( property == PREF_LOG_ON_WARNING ){
+	    			Plugin.INSTANCE.getLogger().setLogOnWarning(newValueAsBoolean);
+	    	  } else if ( property == PREF_LOG_ON_ERROR ){
+	    			Plugin.INSTANCE.getLogger().setLogOnError  (newValueAsBoolean);
+	    	  }
+	       }});
+	}
+
+}
