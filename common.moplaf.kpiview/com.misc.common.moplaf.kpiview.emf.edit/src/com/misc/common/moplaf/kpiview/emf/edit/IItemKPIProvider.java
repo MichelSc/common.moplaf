131199d4a942ad6328828c42b1ada7704295b782
MiSc <MiSc@ORD02652.ordina.belgium>
2017-01-23 08:58:19 +0100

    Add to git

---
+++ common.moplaf.kpiview/com.misc.common.moplaf.kpiview.emf.edit/src/com/misc/common/moplaf/kpiview/emf/edit/IItemKPIProvider.java
@@ -0,0 +1,11 @@
+package com.misc.common.moplaf.kpiview.emf.edit;
+
+import java.util.Collection;
+
+public interface IItemKPIProvider {
+	float getKPIAmount(Object element);
+	float getMinAmount(Object element);
+	float getMaxAmount(Object element);
+	String getKPIID(Object element);  
+	Collection<?> getKPIRanges(Object element);
+}
---
+++ common.moplaf.kpiview/com.misc.common.moplaf.kpiview.emf.edit/src/com/misc/common/moplaf/kpiview/emf/edit/IItemKPIRangeProvider.java
@@ -0,0 +1,6 @@
+package com.misc.common.moplaf.kpiview.emf.edit;
+
+public interface IItemKPIRangeProvider {
+	float getLowAmount(Object element);
+	float getHighAmount(Object element);
+}
