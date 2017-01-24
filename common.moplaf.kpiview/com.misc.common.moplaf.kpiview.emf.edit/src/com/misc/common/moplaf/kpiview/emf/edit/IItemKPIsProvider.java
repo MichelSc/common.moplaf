ce4507231163b5e25422199ab62ecd5562fd2b1f
michel <michelschaffers@gmail.com>
2017-01-24 17:41:31 +0100

    Java doc

--- common.moplaf.kpiview/com.misc.common.moplaf.kpiview.emf.edit/src/com/misc/common/moplaf/kpiview/emf/edit/IItemKPIsProvider.java
+++ common.moplaf.kpiview/com.misc.common.moplaf.kpiview.emf.edit/src/com/misc/common/moplaf/kpiview/emf/edit/IItemKPIsProvider.java
@@ -4,28 +4,29 @@ import java.util.Collection;
 
 /**
  * <p>
- * This interface declare the method(s) to be supported by an object that the Gantt must show as a Row
+ * This interface declare the method(s) to be supported by an object to provide KPI's
  * <p>
  * To implement this interface by an {@link ItemProviderAdapter}, in the concrete class, do the following 
  * <ul>
- *   <li>add <code>@implements IItemIntervalEventsProvider</code> in the user doc</li>
+ *   <li>add <code>@implements IItemKPIsProvider</code> in the user doc</li>
  *   <li>regenerate the code</li>
  *   <li>add the unimplemented methods</li>
- *   <li>implement {@link #getIntervalEvents(Object)}</li>
- *   <li>override {@link #isAdapterForType(Object)} and return true when the type is IItemIntervalEventsProvider</li>
+ *   <li>implement {@link #getKPIs(Object)}</li>
+ *   <li>override {@link #isAdapterForType(Object)} and return true when the type is IItemKPIsProvider</li>
  * </ul> 
  * In the project's ProviderAdapterFactory, you may need to change 
  * <ul>
  *   <li>the method {@link #isFactoryForType} (apparently not)</li>
  *   <li> add the following lines in the constructors (apparenlty yes)
  *     <ul>
- *     <li>	<code>supportedTypes.add(IItemIntervalEventProvider.class); // non emf interface </code></li>
- *     <li>	<code>supportedTypes.add(IItemIntervalEventsProvider.class); // non emf interface </code></li>
+ *     <li>	<code>supportedTypes.add(IItemKPIsProvider.class); // non emf interface </code></li>
+ *     <li>	<code>supportedTypes.add(IItemKPIProvider.class); // non emf interface </code></li>
+ *     <li>	<code>supportedTypes.add(IItemKPIRangeProvider.class); // non emf interface </code></li>
  *     </ul>
  * </ul> 
  * In the plugin.xml,  
  * <ul>
- *   <li>you may need to add the type IItemIntervalEventsProvider in the field SupportedTypes for extension org.eclipse.emf.edit.itemProviderAdapterFactories </li>
+ *   <li>you may need to add the type IItemKPIsProvider in the field SupportedTypes for extension org.eclipse.emf.edit.itemProviderAdapterFactories </li>
  * </ul> 
  * <p>
  * @author michel
