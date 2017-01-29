976fe71c9f5976971a554354274d303789c8a9f0
michel <michelschaffers@gmail.com>
2017-01-29 13:52:55 +0100

    Copyright

--- common.moplaf.emf/com.misc.common.moplaf.emf.editor/src/com/misc/common/moplaf/emf/editor/action/BaseAction.java
+++ common.moplaf.emf/com.misc.common.moplaf.emf.editor/src/com/misc/common/moplaf/emf/editor/action/BaseAction.java
@@ -1,15 +1,14 @@
-package com.misc.common.moplaf.emf.editor.action;
-
-/**
- * Copyright (c) 2002-2007 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2017 Michel Schaffers and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- *   IBM - Initial API and implementation
- */
+ *
+ * Contributors:
+ *     Michel Schaffers - initial API and implementation
+ *******************************************************************************/
+package com.misc.common.moplaf.emf.editor.action;
 
 
 
