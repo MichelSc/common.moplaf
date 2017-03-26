d9cc13d02b3d5644e85f6933f31e1dd45740a7ca
michel <michelschaffers@gmail.com>
2017-03-26 15:19:21 +0200

    Add copyright notice

--- common.moplaf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/common/util/EContainmentListenerAdapter.java
+++ common.moplaf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/common/util/EContainmentListenerAdapter.java
@@ -1,15 +1,15 @@
 package com.misc.common.moplaf.common.util;
 
-/**
- * Copyright (c) 2002-2012 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2017 Michel Schaffers and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- *   IBM - Initial API and implementation
- */
+ *
+ * Contributors:
+ *     Michel Schaffers - initial API and implementation
+ *******************************************************************************/
 
 
 import java.util.Collection;
--- common.moplaf.solver.glpk/com.misc.common.moplaf.solver.glpk.emf/doc/libglpk-java/examples/java/Mip.java
+++ common.moplaf.solver.glpk/com.misc.common.moplaf.solver.glpk.emf/doc/libglpk-java/examples/java/Mip.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Michel Schaffers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Michel Schaffers - initial API and implementation
+ *******************************************************************************/
 import org.gnu.glpk.GLPK;
 import org.gnu.glpk.GLPKConstants;
 import org.gnu.glpk.SWIGTYPE_p_double;
