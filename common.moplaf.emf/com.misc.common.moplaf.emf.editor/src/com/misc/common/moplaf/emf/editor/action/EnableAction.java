7cf7d3057bcd5c70f474ebf76412da5d9aaa113f
michel <michelschaffers@gmail.com>
2017-08-08 22:36:19 +0200

    New actions

---
+++ common.moplaf.emf/com.misc.common.moplaf.emf.editor/src/com/misc/common/moplaf/emf/editor/action/EnableAction.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Michel Schaffers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Michel Schaffers - initial API and implementation
+ *******************************************************************************/
+package com.misc.common.moplaf.emf.editor.action;
+
+
+import java.util.Collection;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.misc.common.moplaf.emf.edit.command.DisableCommand;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.UnexecutableCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.ui.action.StaticSelectionCommandAction;
+
+
+/**
+ * A Enable action  is implemented by creating a {@link EnableCommand}.
+ */
+public class EnableAction extends CommandAction
+{
+	public final static String ID = "com.misc.common.moplaf.emf.editor.action.Enable";
+
+	/**
+	 * This constructs an instance of an action that uses the given editing domain to create a enable command
+	 * for the <code>selection</code> object.
+	 * @since 2.4.0
+	 */
+	public EnableAction(IWorkbenchPart part, ISelection selection)
+	{
+		super(part, selection);
+		this.setId(ID);
+	}
+
+	/**
+	 * This creates the command for {@link StaticSelectionCommandAction#createActionCommand}.
+	 */
+	@Override
+	protected Command createActionCommand(EditingDomain editingDomain, Collection<?> collection)
+	{
+		if (collection.size() == 1)
+		{
+			Object owner = collection.iterator().next();
+			return DisableCommand.create(editingDomain, owner);
+		}
+		return UnexecutableCommand.INSTANCE;
+	}
+}
