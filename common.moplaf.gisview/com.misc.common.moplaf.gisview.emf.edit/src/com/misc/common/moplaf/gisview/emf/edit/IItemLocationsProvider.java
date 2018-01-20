2b53c06383a9c67292f2e00b1d2b4868d642c1c3
MichelSc <michelschaffers@gmail.com>
2018-01-20 12:36:59 +0100

    First version of interface to be consumed by viewer

---
+++ common.moplaf.gisview/com.misc.common.moplaf.gisview.emf.edit/src/com/misc/common/moplaf/gisview/emf/edit/IItemLocationsProvider.java
@@ -0,0 +1,102 @@
+package com.misc.common.moplaf.gisview.emf.edit;
+
+import java.util.Collection;
+
+public interface IItemLocationsProvider {
+
+
+	/**
+	 * <p>
+	 * This interface declares the method(s) to be supported by an object that provides one or 
+	 * locations, for instance to be displayed on a map.
+	 * <p>
+	 * The consumer (i.e. the component using the locations data, typically a map component)
+	 * will provide, when relevant
+	 * <ul>
+	 * <li> an element:Object: the object owning the locations(s) </li>
+	 * <li> 
+	 * a location:Object: an object identifying a location. 
+	 * The object can be an actual Object, or an Integer, if no collection of Location Objects is provided (see further).
+	 * In this latter case, the index is zero-based.
+	 * </li>
+	 * </ul>
+	 * in order to get
+	 * <ul>
+	 * <li> the location(s) supported by the element </li>
+	 * <li> properties to be displayed for a given location </li>
+	 * </ul>
+	 * <p>
+	 * <p>
+	 * @author michel
+	 */
+
+	/**
+	 * Returns the locations published by the element. 
+	 * <p>
+	 * If null is returned, one location is supported by the element, identified by a null key.
+	 * @param element
+	 * @return
+	 */
+	default Collection<?> getLocations(Object element){
+		return null;
+	}
+
+	/**
+	 * Return the text associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default String getLocationText(Object element, Object location) {
+		return "";
+	}
+
+	/**
+	 * Return the color associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default Object getLocationForeground(Object element, Object location) {
+		return null;
+	}
+
+	/**
+	 * Return the image associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default Object getLocationImage(Object element, Object location) {
+		return null;
+	}
+
+	/**
+	 * Return the longitude associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	double getLocationLongitude(Object element, Object location);
+	
+	/**
+	 * Return the longitude associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	double getLocationLatitude(Object element, Object location);
+
+	/**
+	 * Return the longitude associated to a location published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default double getLocationElevation(Object element, Object location) {
+		return 0.0;
+	}
+}
+
+
+
---
+++ common.moplaf.gisview/com.misc.common.moplaf.gisview.emf.edit/src/com/misc/common/moplaf/gisview/emf/edit/IItemPathsProvider.java
@@ -0,0 +1,120 @@
+package com.misc.common.moplaf.gisview.emf.edit;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface IItemPathsProvider {
+
+
+	/**
+	 * <p>
+	 * This interface declares the method(s) to be supported by an object that provides one or 
+	 * paths, for instance to be displayed on a map.
+	 * <p>
+	 * The consumer (i.e. the component using the locations data, typically a map component)
+	 * will provide, when relevant
+	 * <ul>
+	 * <li> an element:Object: the object owning the locations(s) </li>
+	 * <li> an path:Object: the object identifying a path </li>
+	 * <li> an stop:Object: the object identifying a stop of a path </li>
+	 * </ul>
+	 * in order to get
+	 * <ul>
+	 * <li> the paths(s) supported by the element </li>
+	 * <li> properties to be displayed for a given path </li>
+	 * <li> the stops of a path </li>
+	 * <li> properties to be displayed for a given stop of a given path </li>
+	 * </ul>
+	 * <p>
+	 * <p>
+	 * @author michel
+	 */
+
+	/**
+	 * Returns the paths published by the element. 
+	 * <p>
+	 * If null is returned, one path is supported by the element, identified by a null key.
+	 * @param element
+	 * @return
+	 */
+	default Collection<?> getPaths(Object element){
+		return null;
+	}
+
+	/**
+	 * Return the text associated to a path published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default String getPathText(Object element, Object path) {
+		return "";
+	}
+
+	/**
+	 * Return the color associated to a path published by the element.
+	 * @param element
+	 * @param grid
+	 * @return
+	 */
+	default Object getPathForeground(Object element, Object path) {
+		return null;
+	}
+
+	/**
+	 * Return the stops of a path as an ordered collection. 
+	 * <p>
+	 * No collection may be returned. In that case, the consumer will call {@link #getNrPathStops(Object, Object)}
+	 * to get the number of stops and the stop will be identified by an Integer 0-based index.
+	 * @param element
+	 * @param timeline
+	 * @return
+	 */
+	default List<?> getPathStops(Object element, Object path){
+		return null;
+	}
+
+	/** 
+	 * Return the number of stops of a Path
+	 * <p>
+	 * This method will be ignored when {@link #getPathStops(Object, Object)} returns a list.
+	 * @param element
+	 * @param path
+	 * @return
+	 */
+	default int getNrPathStops(Object element, Object path){
+		return 1;
+	}
+
+	/**
+	 * Return the longitude associated to a stop of a path published by the element.
+	 * @param element
+	 * @param path
+	 * @param stop
+	 * @return
+	 */
+	double getPathStopLongitude(Object element, Object path, Object stop);
+	
+	/**
+	 * Return the longitude associated to a stop of a path published by the element.
+	 * @param element
+	 * @param path
+	 * @param stop
+	 * @return
+	 */
+	double getPathStopLatitude(Object element, Object path, Object stop);
+
+	/**
+	 * Return the elevation associated to a stop of path published by the element.
+	 * @param element
+	 * @param path
+	 * @param stop
+	 * @return
+	 */
+	default double getPathStopElevation(Object element, Object location) {
+		return 0.0;
+	}
+}
+
+
+
