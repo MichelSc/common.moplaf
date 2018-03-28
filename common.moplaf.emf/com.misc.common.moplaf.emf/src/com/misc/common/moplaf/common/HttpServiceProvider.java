1b214e7ddc8980c26a5780fdc01a1a509a3e7493
MiSc <MiSc@ORD02652.ordina.belgium>
2018-03-28 12:02:56 +0200

    Work in progress implement http request

---
+++ common.moplaf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/common/HttpServiceProvider.java
@@ -0,0 +1,11 @@
+package com.misc.common.moplaf.common;
+
+
+public interface HttpServiceProvider {
+	
+	public EnabledFeedback getEnabledFeedback();
+	
+	public String getServiceID();
+	
+	public String getObjectID();
+}
