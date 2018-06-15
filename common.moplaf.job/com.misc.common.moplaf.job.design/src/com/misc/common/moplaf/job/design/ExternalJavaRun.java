efe1474be2225daf4ceccad5f141a4f3b7c7d160
MiSc <MiSc@ORD02652.ordina.belgium>
2018-06-15 18:32:39 +0200

    Run menu for design task and docs

--- common.moplaf.job/com.misc.common.moplaf.job.design/src/com/misc/common/moplaf/job/design/ExternalJavaRun.java
+++ common.moplaf.job/com.misc.common.moplaf.job.design/src/com/misc/common/moplaf/job/design/ExternalJavaRun.java
@@ -15,32 +15,47 @@ import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.emf.common.CommonPlugin;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import com.misc.common.moplaf.common.ReturnFeedback;
+import com.misc.common.moplaf.job.Plugin;
+import com.misc.common.moplaf.job.Run;
 
 public class ExternalJavaRun implements IExternalJavaAction {
 
 	public ExternalJavaRun() {
 	}
+	
+	private EList<Run> getRuns(Collection<? extends EObject> objects){
+		
+		BasicEList<Run> runs = new BasicEList<Run>();
+		for ( EObject object : objects) {
+			if ( object instanceof Run ) {
+				Run run = (Run) object;
+				if ( run.getRunFeedback().isEnabled()) {
+					runs.add(run);
+				}
+			}
+		}
+		return runs;
+	}
 
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
-		return true;
+		EList<Run> runs = this.getRuns(arg0);
+		Plugin.INSTANCE.logInfo(String.format("ExternalJavaRun.canExecute: called %d/%d", runs.size(), arg0.size()));
+		return runs.size()>0;
 	}
 
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
-		CommonPlugin.INSTANCE.log("Task And docs.design: test method");
-		Iterator<? extends EObject> iter = arg0.iterator();
-		while (iter.hasNext()){
-			EObject object = iter.next();	
-			CommonPlugin.INSTANCE.log("..object "+object.eClass().getName());
-		}
-		Iterator<Object> paramIter = arg1.values().iterator();
-		while ( paramIter.hasNext() ){
-			Map.Entry<String, Object> next = (Map.Entry<String, Object>)paramIter.next();
-			Object paramValue = next.getValue();
-			CommonPlugin.INSTANCE.log("..param "+next.getKey()+":"+ paramValue  == null ? "null" : paramValue .getClass().getName());
+		EList<Run> runs = this.getRuns(arg0);
+		Plugin.INSTANCE.logInfo(String.format("ExternalJavaRun.execute: called %d/%d", runs.size(), arg0.size()));
+		for ( Run run : runs) {
+			ReturnFeedback feedback = run.run();
+			Plugin.INSTANCE.logInfo(String.format("..run executed success%b feedback=%s", feedback.isSuccess(), feedback.getFeedback()));
 		}
 	}
 
