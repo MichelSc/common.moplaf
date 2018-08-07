fe90b381313d0cd302983a2c655888983b08f79c
ViVn <ViVn@ORD02825.ordina.belgium>
2018-08-07 16:32:08 +0200

    ChartViewerAbstract

--- common.moplaf.chart/com.misc.common.moplaf.chart/src/com/misc/common/moplaf/chart/viewers/ChartViewerAbstract.java
+++ common.moplaf.chart/com.misc.common.moplaf.chart/src/com/misc/common/moplaf/chart/viewers/ChartViewerAbstract.java
@@ -1,5 +1,60 @@
 package com.misc.common.moplaf.chart.viewers;
 
-public class ChartViewerAbstract {
+import org.eclipse.jface.viewers.ContentViewer;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Control;
+
+import com.misc.common.moplaf.timeview.IIntervalEventProvider;
+
+public class ChartViewerAbstract extends ContentViewer {
+
+	private IColorProvider colorProvider = null;
+	private IIntervalEventProvider intervalEventProvider = null;
+
+	public void setIntervalEventProvider(IIntervalEventProvider eventProvider){
+		this.intervalEventProvider = eventProvider;
+	}
+
+	protected IIntervalEventProvider getIIntervalEventProvider(){
+		return this.intervalEventProvider;
+	}
+	
+	public void setColorProvider(IColorProvider provider) {
+		this.colorProvider = provider;
+	}
+
+	protected ILabelProvider getILabelProvider(){
+		return (ILabelProvider)this.getLabelProvider();
+	}
+	
+	protected IColorProvider getIColorProvider(){
+		return this.colorProvider;
+	}
+	
+	@Override
+	public Control getControl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ISelection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void refresh() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSelection(ISelection selection, boolean reveal) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
