468a6f41bb11e69f6806b3f3560502b4c5e7b9f4
ViVn <ViVn@ORD02825.ordina.belgium>
2018-08-09 10:15:56 +0200

    .

--- common.moplaf.chart.swtchart/com.misc.common.moplaf.chart.swtchart/src/com/misc/common/moplaf/chart/swtchart/ChartView.java
+++ common.moplaf.chart.swtchart/com.misc.common.moplaf.chart.swtchart/src/com/misc/common/moplaf/chart/swtchart/ChartView.java
@@ -1,5 +1,16 @@
 package com.misc.common.moplaf.chart.swtchart;
 
-public class ChartView {
+import org.eclipse.swt.widgets.Composite;
+
+import com.misc.common.moplaf.chart.emf.editor.views.ChartViewAbstract;
+import com.misc.common.moplaf.chart.viewers.ChartViewerAbstract;
+
+public class ChartView extends ChartViewAbstract {
+
+	@Override
+	protected ChartViewerAbstract createViewer(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
