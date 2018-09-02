2f9eb304cb9680d00cf35717d78f9651e3fdefac
michel <michel@michel-tower>
2018-09-02 11:38:40 +0200

    Rework analysis setup dialog

--- common.moplaf.datatools/com.misc.common.moplaf.analysis.emf/src/com/misc/common/moplaf/analysis/util/AnalysisServices.java
+++ common.moplaf.datatools/com.misc.common.moplaf.analysis.emf/src/com/misc/common/moplaf/analysis/util/AnalysisServices.java
@@ -35,19 +35,13 @@ public class AnalysisServices {
     	}
   		return new_tools;
     }
-    
-    public EList<DataTool> getNewCategorizers(AnalysisDomain domain, EClass element_type) {
-    	EList<DataTool> new_tools = domain.getNewDataTools(DataToolType.CATEGORIZER);
-    	Iterator<DataTool> iterator = new_tools.iterator();
-    	while( iterator.hasNext() ) {
-    		Categorizer categorizer = (Categorizer) iterator.next();
-    		if ( !categorizer.isValidElementType(element_type) ) {
-    			iterator.remove();
-    		}
-    	}
-  		return new_tools;
+
+    public EList<DataTool> getNewExtractors(DocAnalysis analysis) {
+    	AnalysisDomain domain = analysis.getDomain();
+    	Doc doc = analysis.getDoc();
+    	return this.getNewExtractors(domain, doc);
     }
-    
+
     public EList<DataTool> getNewColumnizers(AnalysisDomain domain, EClass element_type) {
     	EList<DataTool> new_tools = domain.getNewDataTools(DataToolType.COLUMNIZER);
     	Iterator<DataTool> iterator = new_tools.iterator();
@@ -60,11 +54,53 @@ public class AnalysisServices {
   		return new_tools;
     }
     
+    public EList<DataTool> getNewColumnizers(DocAnalysis analysis) {
+    	AnalysisDomain domain = analysis.getDomain();
+    	Extractor extractor = analysis.getExtractor();
+    	if ( extractor == null ) { return null; }
+    	EClass type = extractor.getExtractedType();
+    	if ( type == null ) { return null; }
+    	return this.getNewColumnizers(domain, type);
+    }
+    
+    public EList<DataTool> getNewCategorizers(DocAnalysis analysis) {
+    	AnalysisDomain domain = analysis.getDomain();
+    	Extractor extractor = analysis.getExtractor();
+    	if ( extractor == null ) { return null; }
+    	EClass type = extractor.getExtractedType();
+    	if ( type == null ) { return null; }
+    	return this.getNewCategorizers(domain, type);
+    }
+    
+
+
+    public EList<DataTool> getNewCategorizers(AnalysisDomain domain, EClass element_type) {
+    	EList<DataTool> new_tools = domain.getNewDataTools(DataToolType.CATEGORIZER);
+    	Iterator<DataTool> iterator = new_tools.iterator();
+    	while( iterator.hasNext() ) {
+    		Categorizer categorizer = (Categorizer) iterator.next();
+    		if ( !categorizer.isValidElementType(element_type) ) {
+    			iterator.remove();
+    		}
+    	}
+  		return new_tools;
+    }
+    
     public boolean docAnalysisSetDataTools(DocAnalysis analysis, Extractor extractor, List<ColumnizerAbstract> columnizers, List<Categorizer> categorizers) {
     	analysis.setExtractor(extractor);
     	analysis.getColumnizers().clear();
     	analysis.getColumnizers().addAll(columnizers);
     	analysis.getCategorizers().clear();
+    	analysis.getCategorizers().addAll(categorizers);
+    	return true;
+    }
+
+    public boolean docAnalysisAddColumnizers(DocAnalysis analysis, List<ColumnizerAbstract> columnizers) {
+    	analysis.getColumnizers().addAll(columnizers);
+    	return true;
+    }
+
+    public boolean docAnalysisAddCategorizers(DocAnalysis analysis, List<Categorizer> categorizers) {
     	analysis.getCategorizers().addAll(categorizers);
     	return true;
     }
