34c5778c5bad37e082062e1b198e6e83e0cfd74d
MiSc <MiSc@ORD02652.ordina.belgium>
2018-09-16 11:06:28 +0200

    Implement CompoundPropertiesProvider

--- common.moplaf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/common/util/PropertiesProvider.java
+++ common.moplaf.emf/com.misc.common.moplaf.emf/src/com/misc/common/moplaf/common/util/PropertiesProvider.java
@@ -30,9 +30,6 @@ public class PropertiesProvider implements IPropertiesProvider {
 		public int getPropertyDispalyWidth();
 	};
 	
-	public String getText() {
-		return "";
-	}
 	/**
 	 * 
 	 */
--- common.moplaf.gridview/com.misc.common.moplaf.gridview.emf.edit/src/com/misc/common/moplaf/gridview/emf/edit/util/PropertiesProviderGridsProvider.java
+++ common.moplaf.gridview/com.misc.common.moplaf.gridview.emf.edit/src/com/misc/common/moplaf/gridview/emf/edit/util/PropertiesProviderGridsProvider.java
@@ -19,6 +19,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 
 import com.misc.common.moplaf.common.IPropertiesProvider;
+import com.misc.common.moplaf.common.util.CompoundPropertiesProvider;
 import com.misc.common.moplaf.gridview.emf.edit.IItemGridsProvider;
 
 public class PropertiesProviderGridsProvider implements IItemGridsProvider {
@@ -31,53 +32,59 @@ public class PropertiesProviderGridsProvider implements IItemGridsProvider {
 
 	public interface SheetDelegate {
 		public String getSheetText();
+		public int getTraits();
 		public Collection<?> getRows(Object sheet);
 		public String getRowText(Object row);
 		public Collection<?> getColumns();
-		public int getTraits();
-	}
-	
-	private class IPropertiesProviderProperty{
-		private IPropertiesProvider provider;
-		private Object property;
-		public IPropertiesProviderProperty(IPropertiesProvider provider, Object property) {
-			super();
-			this.provider = provider;
-			this.property = property;
-		}
+		public int getNrColumns();
+		public String getColumnText(Object column);
+		public int getColumnType(Object column);
+		public int getColumnWidth(Object column);
+		public Object getCellValue(Object row, Object column);
 	}
 	
 	private class SheetFeature implements SheetDelegate{
 		
 		private EReference[] path;
 		private EAttribute attribute;
-		private IPropertiesProvider[] providers;
+		private IPropertiesProvider provider;
 		private int traits;
 		private String sheet_name;
 		
-		public SheetFeature(String sheet_name, EReference[] path, EAttribute attribute, IPropertiesProvider[] columns, int traits) {
+		public SheetFeature(String sheet_name, EReference[] path, EAttribute attribute, IPropertiesProvider provider, int traits) {
 			this.path = path;
 			this.attribute = attribute;
-			this.providers = columns;
+			this.provider = provider;
+			this.traits = traits;
+			this.sheet_name = sheet_name;
+		}
+		public SheetFeature(String sheet_name, EReference[] path, EAttribute attribute, IPropertiesProvider[] providers, int traits) {
+			this.path = path;
+			this.attribute = attribute;
+			this.provider = new CompoundPropertiesProvider(providers);
 			this.traits = traits;
 			this.sheet_name = sheet_name;
 		}
 		@Override
-		public Collection<?> getRows(Object element) {
-			if ( this.path.length==0 ) {
-				return null;
-			}
-			if ( !(element instanceof EObject) ) {
-				return null;
+		public String getSheetText() {
+			if ( this.sheet_name!=null ) {
+				return this.sheet_name;
 			}
 			
-			EObject object = (EObject)element;
-			HashSet<EObject> result = new HashSet<EObject>();
-			this.collectRows(object, result, 0);
-			
-			return result;
+			// compose a name from the refs at hand
+			String path_asstring = "";
+			for ( int i=0; i<this.path.length; i++) {
+				if ( path_asstring.length()>0 ) {
+					path_asstring += "/";
+				}
+				path_asstring+= this.path[i].getName();  
+			}
+			return path_asstring;
 		}
-		
+		@Override
+		public int getTraits() {
+			return this.traits;
+		}
 		private void collectRows(EObject from, HashSet<EObject> result, int level) {
 			EReference reference = this.path[level];
 			if ( reference.getEContainingClass().isSuperTypeOf(from.eClass())) {
@@ -102,6 +109,22 @@ public class PropertiesProviderGridsProvider implements IItemGridsProvider {
 				}
 			}
 		}
+		@Override
+		public Collection<?> getRows(Object element) {
+			if ( this.path.length==0 ) {
+				return null;
+			}
+			if ( !(element instanceof EObject) ) {
+				return null;
+			}
+			
+			EObject object = (EObject)element;
+			HashSet<EObject> result = new HashSet<EObject>();
+			this.collectRows(object, result, 0);
+			
+			return result;
+		}
+		
 		
 		@Override
 		public String getRowText(Object row) {
@@ -113,51 +136,33 @@ public class PropertiesProviderGridsProvider implements IItemGridsProvider {
 			}
 			EObject object = (EObject)row;
 			return object.eGet(this.attribute).toString();
-		}
-	
+		}	
 		@Override
 		public Collection<?> getColumns() {
-			LinkedList<Object> columns = new LinkedList<>();
-			for ( int i=0; i<this.providers.length; i++) {
-				IPropertiesProvider provider = this.providers[i];
-				Collection<?> properties = provider.getProperties();
-				if ( properties!=null) {
-					for ( Object property : properties) {
-						columns.add(new IPropertiesProviderProperty(provider, property));
-					}
-				} else {
-					int nr_properties = provider.getNrProperties();
-					for (int property=0; property<nr_properties; property++) {
-						columns.add(new IPropertiesProviderProperty(provider, property));
-					}
-				}
-			}
-			return columns;
+			return this.provider.getProperties();
 		}
-		
 		@Override
-		public String getSheetText() {
-			if ( this.sheet_name!=null ) {
-				return this.sheet_name;
-			}
-			
-			// compose a name from the refs at hand
-			String path_asstring = "";
-			for ( int i=0; i<this.path.length; i++) {
-				if ( path_asstring.length()>0 ) {
-					path_asstring += "/";
-				}
-				path_asstring+= this.path[i].getName();  
-			}
-			return path_asstring;
+		public int getNrColumns() {
+			return this.provider.getNrProperties();
 		}
-	
 		@Override
-		public int getTraits() {
-			return this.traits;
+		public String getColumnText(Object column) {
+			return this.provider.getPropertyText(column);
+		}
+		@Override
+		public int getColumnType(Object column) {
+			return this.provider.getPropertyType(column);
+		}
+		@Override
+		public int getColumnWidth(Object column) {
+			return this.provider.getPropertyDisplayWidth(column);
+		}
+		@Override
+		public Object getCellValue(Object row, Object column) {
+			return this.provider.getPropertyValue(row,  column);
 		}
 	}
-
+	
 	private PropertiesProviderGridsProvider() {
 		super();
 		this.sheets = new LinkedList<SheetDelegate>();
@@ -183,11 +188,11 @@ public class PropertiesProviderGridsProvider implements IItemGridsProvider {
 		return this;
 	}
 	public PropertiesProviderGridsProvider addSheet(String sheet_name, EReference[] references, EAttribute attribute, IPropertiesProvider provider, int traits) {
-		this.sheets.add(new SheetFeature(sheet_name, references,  attribute, new IPropertiesProvider[] {provider}, traits));
+		this.sheets.add(new SheetFeature(sheet_name, references,  attribute, provider, traits));
 		return this;
 	}
 	public PropertiesProviderGridsProvider addSheet(String sheet_name, EReference reference, EAttribute attribute, IPropertiesProvider provider, int traits) {
-		this.sheets.add(new SheetFeature(sheet_name, new EReference[] {reference},  attribute, new IPropertiesProvider[] {provider}, traits));
+		this.sheets.add(new SheetFeature(sheet_name, new EReference[] {reference},  attribute, provider, traits));
 		return this;
 	}
 	public PropertiesProviderGridsProvider addSheet(String sheet_name, EReference[] references, EAttribute attribute, IPropertiesProvider[] providers) {
@@ -243,27 +248,32 @@ public class PropertiesProviderGridsProvider implements IItemGridsProvider {
 	}
 	
 	@Override
+	public int getNrColumns(Object element, Object grid) {
+		SheetDelegate delegate = (SheetDelegate)grid;
+		return delegate.getNrColumns();
+	}
+	
+	@Override
 	public String getColumnText(Object element, Object grid, Object column) {
-		IPropertiesProviderProperty property_column = (IPropertiesProviderProperty)column;
-		IPropertiesProvider provider = property_column.provider;
-		Object property = property_column.property;
-		return provider.getPropertyText(property);
+		SheetDelegate delegate = (SheetDelegate)grid;
+		return delegate.getColumnText(column);
 	}
 	
 	@Override
 	public int getColumnWidth(Object element, Object grid, Object column) {
-		IPropertiesProviderProperty property_column = (IPropertiesProviderProperty)column;
-		IPropertiesProvider provider = property_column.provider;
-		Object property = property_column.property;
-		return provider.getPropertyDisplayWidth(property);
+		SheetDelegate delegate = (SheetDelegate)grid;
+		return delegate.getColumnWidth(column);
 	}
 
 	@Override
 	public Object getCellValue(Object element, Object grid, Object row, Object column) {
-		IPropertiesProviderProperty property_column = (IPropertiesProviderProperty)column;
-		IPropertiesProvider provider = property_column.provider;
-		Object property = property_column.property;
-		return provider.getPropertyValue(row, property);
+		SheetDelegate delegate = (SheetDelegate)grid;
+		return delegate.getCellValue(row, column);
+	}
+	@Override
+	public int getCellType(Object element, Object grid, Object row, Object column) {
+		SheetDelegate delegate = (SheetDelegate)grid;
+		return delegate.getColumnType(column);
 	}
 
 }
